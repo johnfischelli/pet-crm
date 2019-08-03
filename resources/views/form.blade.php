631837c6c70cefa91c275df4a17065907de39599
John Fischelli <john.fischelli@gmail.com>
2019-08-03 08:29:05 -0500

    more mobile friendly changes

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -67,13 +67,13 @@
             <div class="w-full px-3">
               <h2 class="text-xl text-gray-700 border-b border-pink-500 my-3">Add Appointment</h2>
             </div>
-            <div class="w-full md:w-2/3 px-3">
+            <div class="w-2/3 px-3">
               @include('components.date-input',[ 'id' => 'appointment_date', 'label' => 'Date', 'value' => null ])
             </div>
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               @include('components.select-input',[ 'id' => 'appointment_type', 'label' => 'Type', 'values' => ['Walk' => 'walk', 'Pet Sit' => 'pet-sit'], 'selected' => null])
             </div>
-            <div class="w-full md:w-2/3 px-3">
+            <div class="w-2/3 px-3">
               @include('components.textarea-input',[ 'id' => 'appointment_notes', 'label' => 'Notes', 'value' => null])
             </div>
           </div>
@@ -82,46 +82,46 @@
             <div class="w-full px-3 mb-5">
               <h2 class="text-xl text-gray-700 border-b border-pink-500 my-3">Edit Customer</h2>
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'owner_first_name', 'label' => 'Owner First Name', 'value' => $customer->owner_first_name ])
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'owner_last_name', 'label' => 'Owner Last Name', 'value' => $customer->owner_last_name ])
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'owner_email', 'label' => 'Owner Email', 'value' => $customer->owner_email ])
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'owner_phone', 'label' => 'Owner Phone', 'value' => $customer->owner_phone ])
             </div>
           </div>
 
           <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               @include('components.text-input',[ 'id' => 'pet_name', 'label' => 'Pet Name', 'value' => $customer->pet_name ])
             </div>
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               @include('components.text-input',[ 'id' => 'pet_type', 'label' => 'Pet Type', 'value' => $customer->pet_type ])
             </div>
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               @include('components.text-input',[ 'id' => 'pet_breed', 'label' => 'Pet Breed', 'value' => $customer->pet_breed ])
             </div>
           </div>
 
           <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'address', 'label' => 'Address', 'value' => $customer->address ])
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div class="w-1/2 px-3">
               @include('components.text-input',[ 'id' => 'address_line_2', 'label' => 'Address Line 2', 'value' => $customer->address_line_2 ])
             </div>
           </div>
 
           <div class="flex flex-wrap -mx-3 mb-2">
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               @include('components.text-input',[ 'id' => 'city', 'label' => 'City', 'value' => $customer->city ])
             </div>
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-1/3 px-3">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="state">
                 State
               </label>
@@ -134,7 +134,7 @@
                 </div>
               </div>
             </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div class="w-1/3 px-3 mb-6 sm:mb-0">
               @include('components.text-input',[ 'id' => 'zip', 'label' => 'Zip', 'value' => $customer->zip ])
             </div>
           </div>
